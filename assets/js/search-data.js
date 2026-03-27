8418ed4e24f72228b492633189f7c66cecde7b30
ggiorgis <ggiorgis@student.ethz.ch>
2026-03-27 18:02:00 +0000

    Deploying to gh-pages from @ ggiorgis/ggiorgis.github.io@bbe726f48d312962db8ebdc119f122dc30d56b3b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,19 +9,26 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-bookshelf",
-          title: "bookshelf",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/books/";
-          },
-        },{id: "nav-cv",
+  },{id: "nav-cv",
           title: "CV",
           description: "A brief overview of my CV; for the full version, please refer to the PDF.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
+          },
+        },{id: "nav-resources",
+          title: "Resources",
+          description: "On this page, I gather and share a curated list of resources—books, articles, tools, websites, and ideas—that have inspired me, deepened my understanding, or simply sparked my curiosity. These are the things I truly enjoy and wholeheartedly recommend to others. If you know of any resources that should be added or have suggestions to make this collection better, I warmly invite you to get in touch and share your favorites!",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/resources/";
+          },
+        },{id: "nav-blog",
+          title: "Blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
           },
         },{id: "books-the-godfather",
           title: 'The Godfather',
